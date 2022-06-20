31339a2335ec3a310f411062deb5b74b938a3202
Diego Luna <slowkingvii@gmail.com>
2022-06-20 16:36:50 -0500

    Add 'Event Listener' to menu links to close the menu

--- mobile_menu.js
+++ mobile_menu.js
@@ -2,18 +2,23 @@ const btnClose = document.getElementById('nav-close');
 const btnOpen = document.getElementById('nav-open');
 const linksContainer = document.querySelector('.links-container');
 const header = document.getElementById('toolbar');
+const menuItems = document.querySelectorAll('.nav-links');
 
 const toolbarStyle = () => {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) header.classList.add('header-bg');
     else header.classList.remove('header-bg');
 }
-
-btnClose.addEventListener('click', () => {
+const closeMenu = () => {
     linksContainer.classList.remove('show-container');
-});
-
-btnOpen.addEventListener('click', () => {
+}
+const openMenu = () => {
     linksContainer.classList.add('show-container');
+}
+
+btnClose.addEventListener('click', closeMenu);
+btnOpen.addEventListener('click', openMenu);
+menuItems.forEach(item => {
+    item.addEventListener('click', closeMenu);
 });
 
 window.onscroll = toolbarStyle;
