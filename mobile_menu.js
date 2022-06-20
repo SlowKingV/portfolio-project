b7ad72678879b426be216f773d98f76b0c33ca8c
Diego Luna <slowkingvii@gmail.com>
2022-06-20 16:28:11 -0500

    Modify toolbar background on scroll

--- mobile_menu.js
+++ mobile_menu.js
@@ -1,6 +1,12 @@
 const btnClose = document.getElementById('nav-close');
 const btnOpen = document.getElementById('nav-open');
 const linksContainer = document.querySelector('.links-container');
+const header = document.getElementById('toolbar');
+
+const toolbarStyle = () => {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) header.classList.add('header-bg');
+    else header.classList.remove('header-bg');
+}
 
 btnClose.addEventListener('click', () => {
     linksContainer.classList.remove('show-container');
@@ -9,3 +15,6 @@ btnClose.addEventListener('click', () => {
 btnOpen.addEventListener('click', () => {
     linksContainer.classList.add('show-container');
 });
+
+window.onscroll = toolbarStyle;
+window.onload = toolbarStyle;
