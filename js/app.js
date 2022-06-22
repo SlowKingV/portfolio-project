9f7b75e7b3d9111657104eece1aa594854e85e18
Diego Luna <slowkingvii@gmail.com>
2022-06-22 11:09:08 -0500

    Add modal window functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/* Define elements needed for interaction */
+// Define elements needed for interaction
 const btnOpen = document.getElementById('nav-open');
 const linksContainer = document.querySelector('.links-container');
 const header = document.getElementById('toolbar');
@@ -11,7 +11,7 @@ document.querySelectorAll('.nav-links').forEach((item, index) => {
   closeMenuItems[`navLink${index}`] = item;
 });
 
-/* Page interaction functions */
+// Page interaction functions
 const toolbarStyle = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) header.classList.add('header-bg');
   else header.classList.remove('header-bg');
@@ -23,25 +23,26 @@ const setToActive = () => {
   linksContainer.classList.add('active');
 };
 
-/* Add listener to elements */
+// Add listener to elements
 btnOpen.addEventListener('click', setToActive);
 for (let item in closeMenuItems) {
   let element = closeMenuItems[item];
   element.addEventListener('click', setToInactive);
 }
 
-/* Toolbar update */
+// Toolbar update
 window.onscroll = toolbarStyle;
 window.onload = toolbarStyle;
 
-/* WORKS SECTION POPULATE */
-/* Data to use */
+// WORKS SECTION POPULATE
+// Data to use
 const data = [
   {
     "index": 1,
     "name": "Tonic",
     "info": ["CANOPY", "Back End Dev", "2015"],
     "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    "details": "A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     "tags": ["html", "css", "javaScript"]
   },
   {
@@ -49,6 +50,7 @@ const data = [
     "name": "Multi-Post Stories",
     "info": ["FACEBOOK", "Full Stack Dev", "2015"],
     "description": "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    "details": "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     "tags": ["html", "Ruby on rails", "css", "javaScript"]
   },
   {
@@ -56,6 +58,7 @@ const data = [
     "name": "Facebook 360",
     "info": ["FACEBOOK", "Full Stack Dev", "2015"],
     "description": "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    "details": "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     "tags": ["html", "Ruby on rails", "css", "javaScript"]
   },
   {
@@ -63,17 +66,27 @@ const data = [
     "name": "Uber Navigation",
     "info": ["Uber", "Lead Developer", "2018"],
     "description": "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    "details": "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     "tags": ["html", "Ruby on rails", "css", "javaScript"]
+  },
+  {
+    "index": 5,
+    "name": "Test Project Card",
+    "info": ["Enterprise", "Lead Developer", "2022"],
+    "description": "Test description for this project card.",
+    "details": "Test description for this project card. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    "tags": ["html", "Ruby on rails", "css", "javaScript", "Python"]
   }
 ];
 
-/* Build HTML for each card as needed */
+// Build HTML for each card as needed
 const createHtmlCard = (cardData) => {
   let infoElements = '';
   let tagElements = '';
 
-  cardData.info.forEach((value) => {
-    infoElements = infoElements.concat(`      <li class="info-item">${value}</li><li class="dot"></li>\n`);
+  cardData.info.forEach((value, index) => {
+    if (index > 0) infoElements = infoElements.concat('<li class="dot"></li>\n')
+    infoElements = infoElements.concat(`      <li class="info-item">${value}</li>`);
   });
   cardData.tags.forEach((value) => {
     tagElements = tagElements.concat(`      <li>${value}</li>\n`)
@@ -81,7 +94,7 @@ const createHtmlCard = (cardData) => {
 
   let htmlTemplate = `
 <section class="card">
-  <a href="#"><img src="assets/work_${cardData.index}.png" alt="Preview of the project ${cardData.name}"></a>
+  <a href="javascript:void(0)" class="card-img-link" data-index="${cardData.index}"><img src="assets/work_${cardData.index}.png" alt="Preview of the project ${cardData.name}"></a>
   <div class="card-content">
     <h2>${cardData.name}</h2>
     <ul class="info">
@@ -91,7 +104,7 @@ ${infoElements}
     <ul class="tags">
 ${tagElements}
     </ul>
-    <button type="button" class="btn-more">See Project</button>
+    <button type="button" class="btn-more card-btn" data-index="${cardData.index}">See Project</button>
   </div>
 </section>
 `;
@@ -99,7 +112,7 @@ ${tagElements}
   return htmlTemplate;
 }
 
-/* Create a card for each entry in 'data' and save it on a single string */
+// Create a card for each entry in 'data' and save it on a single string
 let workSectionHtml = "";
 
 data.forEach((cardData) => {
@@ -107,3 +120,55 @@ data.forEach((cardData) => {
 });
 
 worksSection.innerHTML = workSectionHtml;
+
+// MODAL WINDOW
+
+const seeMoreBtns = Array.from(document.getElementsByClassName('card-btn'))
+  .concat(Array.from(document.getElementsByClassName('card-img-link')));
+const modalPopup = document.getElementById('project-wrapper');
+
+const insertProjectData = (data) => {
+  const name = document.getElementById('project-name');
+  const info = document.getElementById('project-info');
+  const img = document.getElementById('project-img');
+  const details = document.getElementById('project-details');
+  const tags = document.getElementById('project-tags');
+
+  let infoElements = "";
+  let tagElements = "";
+  data.info.forEach((value, index) => {
+    if (index > 0) infoElements = infoElements.concat('<li class="dot"></li>\n')
+    infoElements = infoElements.concat(`<li class="info-item">${value}</li>`);
+  });
+  data.tags.forEach((value) => {
+    tagElements = tagElements.concat(`<li>${value}</li>\n`)
+  });
+
+  name.textContent = data.name;
+  info.innerHTML = infoElements;
+  img.setAttribute('src', `./assets/work_${data.index}.png`);
+  img.setAttribute('alt', `Preview of the project ${data.name}`);
+  details.textContent = data.details;
+  tags.innerHTML = tagElements;
+}
+
+const openProject = (event) => {
+  insertProjectData(data[event.currentTarget.dataset.index - 1]);
+  modalPopup.classList.remove('d-none');
+  setTimeout(() => {modalPopup.classList.add('active')}, 0);
+}
+
+seeMoreBtns.forEach(button => {
+  button.addEventListener('click', openProject);
+})
+
+const projectCloseBtn = document.getElementById('project-close');
+const closeProject = (event) => {
+  if (event.target !== event.currentTarget) return;
+  // document.body.classList.remove('stop-scroll');
+  modalPopup.classList.remove('active');
+  setTimeout(() => {modalPopup.classList.add('d-none')}, 300);
+}
+
+projectCloseBtn.addEventListener('click', closeProject);
+modalPopup.addEventListener('click', closeProject);
